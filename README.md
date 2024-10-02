7314874d59160374f23361a0dc7dcceafaaea8f1
Anilkumar Reddy <anilkreddy.a@gmail.com>
2024-10-02 01:19:40 -0400

    Astro first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SAP CX Actions: Docs
+
+This repository contains the documentation for the SAP Commerce Cloud in the Public Cloud (CCv2) GitHub Actions.
---
+++ astro.config.mjs
@@ -0,0 +1,52 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+export const locales = {
+	root: { label: 'English', lang: 'en' },
+	fr: { label: 'Français', lang: 'fr' },
+}
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'SAP CX Actions',
+			social: {
+				github: 'https://github.com/sap-cx-actions/docs',
+			},
+			customCss: ['./src/assets/landing.css'],
+			defaultLocale: 'root',
+			locales,
+			sidebar: [
+				{
+					label: 'Hello, World!',
+					translations: {
+						fr: 'Bonjour, le monde!',
+					},
+					items: [
+						{
+							label: 'Getting Started',
+							slug: 'hello-world'
+						},
+					],
+				},
+				{
+					label: 'Commerce Cloud',
+					autogenerate: { directory: 'commerce-cloud' },
+				},
+				{
+					label: 'Reference',
+					items: [
+						{
+							label: 'API Reference',
+							slug: 'reference/api'
+						},
+						{
+							label: 'CLI Reference',
+							slug: 'reference/cli'
+						},
+					]
+				}
+			],
+		}),
+	],
+});
